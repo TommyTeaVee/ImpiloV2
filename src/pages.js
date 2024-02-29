65600a78bff2d8420f76b7162d726c97c766ff1c
TommyTeaVee <fihlatv@gmail.com>
2024-03-01 01:49:34 +0200

    final_designs

--- src/pages.js
+++ src/pages.js
@@ -21,11 +21,11 @@ export const pages = [
     "sample3.png?auto=compress&cs=tinysrgb&dpr=1&w=1506",
     "color_graph.png?auto=compress&cs=tinysrgb&dpr=1&w=1506",
     "19.png?auto=compress&cs=tinysrgb&dpr=1&w=1506",
-    "taylor-grote.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1506",
+   /* "taylor-grote.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=1506",
     "BackCover.png?auto=compress&cs=tinysrgb&dpr=1&w=1506",
 
    // "fashion.png?auto=compress&cs=tinysrgb&dpr=1&w=1506",
-  /*   "dna.png?auto=compress&cs=tinysrgb&dpr=1&w=100",
+     "dna.png?auto=compress&cs=tinysrgb&dpr=1&w=100",
     "dna_cov.png?auto=compress&cs=tinysrgb&dpr=1&w=100",
     "catalogue.png?auto=compress&cs=tinysrgb&dpr=1&w=1506", */
    // "https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA2L3M3MS1tY2tpbnNleS0xNDkxLWZvbi5qcGc.jpg?auto=compress&cs=tinysrgb&dpr=1&w=1506"
